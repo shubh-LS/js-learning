2bbd516f26ff57fa0fc29ad735a5b428e7db81cf
SHUBH AGRAWAL <11shubhagrawal@gmail.com>
2026-08-31 07:00:22 +0000

    learned  data types

--- .devcontainer/01_basics/text.js
+++ .devcontainer/01_basics/text.js
@@ -1,1 +1,28 @@
+"use strict" ;//treat all js code as newer version
+// alert("hello")   we are using nodejs therefore no alert use
 console.log("shubh") 
+const accountId =  144553
+let accountEmail = "11shubhagrawal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+account
+/*
+prefer not to use var cuz of issue of block scope and functional scope
+*/
+// accountId = 2        not allowed
+console.log(accountId) ;
+console.table([accountId , accountEmail , accountPassword , accountCity ]) ;
+
+let name = "shubh"
+let age = 19 
+let isLoggedIn = false
+// number => 2 to the power of 53
+//bigint
+//string can be used with ' and " both
+//boolean => true or false
+// null => standalone value
+// undefined => variable value not assigned
+// symbol => unique
+//object 
+console.log(typeof isLoggedIn) ;
+console.log(typeof null) ;    //null is an object in js but it is a primitive data type
