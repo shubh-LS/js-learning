2d9be90b007ee2ef388ac36a70db7f3d6f7c5870
SHUBH AGRAWAL <11shubhagrawal@gmail.com>
2026-09-04 13:59:17 +0000

    1st part done

--- .devcontainer/01_basics/text.js
+++ .devcontainer/01_basics/text.js
@@ -38,5 +38,35 @@ console.log(valueInNumber) ;
 //true => 1 ; false => 0 
 //"" => false ; " " => true ; "xyz" => true
 
+//  ***************operations********************
+let value = 3
+let negValue = -value   
+
+console.log(3**2) ; //3**2 = 9
+
+console.log(2 + 3 +"3")  // 53
+console.log(2 + "3")    // 23
+
+// == & === do type ke hote h iidhar
 
 
+// stack(primitive - copy) , heap memory (non primitive - reference)
+
+console.log("shubh")
+const names = "shubh"
+const repoCount = 50
+console.log(`hello my name is ${names} annd my repo count is ${repoCount}`); 
+
+console.log(names.__proto__);          
+const balance = new Number(100)  //object
+
+console.log(balance.toString.length)  // 3
+console.log(balance.toFixed(2))  // 100.00
+
+//  ***************dates(object)********************
+
+let myDate = new Date()
+console.log(myDate.toString()) ;
+
+let myTime = Date.now()
+console.log(myTime) ;   
